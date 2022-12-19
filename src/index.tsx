8cdfd1821dc2ffa1db4a4c63918da27b71fe5f50
Irakli Kverenchkhiladze <irakli_kverenchkhiladze@epam.com>
2022-12-19 22:53:54 +0400

    added fancylogger

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import fancyLogger from '@spaceworm/fancy-logger';
+
+fancyLogger.green('Happy Hacking...');
+fancyLogger.red('Space Worm!');
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
